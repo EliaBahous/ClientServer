03d481b2f9a05ebee499049f9187e5a0f54a72d7
Elia Bahous <ebahous5@gmail.com>
2020-12-25 17:54:28 +0200

    fixing postgresql

--- Server.js
+++ Server.js
@@ -2,26 +2,24 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 var bodyParser = require('body-parser');
-var mysql = require('mysql'); 
+const {Pool, Client} = require('pg')
+
+const connectionString = 'postgressql://postgres:wasap@localhost:5432/Store';
+
+const client = new Client(connectionString);
+client
+  .connect()
+  .then(() => console.log('connected'))
+  .catch(err => console.error('connection error', err.stack));
 var sessions = [];
 app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 let port = process.env.PORT || 3000;
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: 'store'
+app.listen(port, function(){
+  console.log("Listening on port 3000!");
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-app.listen(port, function(){
-  console.log("Listening on port 3000!")
-});
 
 /************************Functions for request************************/
 
@@ -70,7 +68,7 @@ app.get('/logout', function(req, res) {
   res.sendFile(__dirname+ "/Website/login.html");
 });
 function insertClient(req,res){
-  con.query("SELECT idusers,name,familyname FROM users WHERE Email='"+user_id+"' AND Password='"+pass+"'", function (err, result, fields) {
+  pool.query("SELECT id,name,familyname FROM store.users WHERE Email='"+user_id+"' AND Password='"+pass+"'", (err, result)=>{
     if (err) throw err;
     if(result.length > 0){
 
@@ -93,7 +91,7 @@ app.get('/getdata',function detData(req,res){
   for(i=0;i<sessions.length && flag == 0;i++){
     console.log( " -- " +sessions[i].email );
     if(req.session && sessions[i].email == req.session.email){
-      res.send(sessions[i].name + " " +sessions[i].lname);
+      res.send(sessions[i].name + " " +sessions[i].familyname);
       flag=1;
     }
   }
@@ -101,8 +99,8 @@ app.get('/getdata',function detData(req,res){
 
 function checkClient(req,res) {
   sess = req.session;
-  var user_id = req.body.id;
-  var pass = req.body.pass;
+  var user_id = req.body.id + '';
+  var pass = req.body.pass + '';
   var i=0;
   var flag = 0;
   console.log("current request: " +req.session.email);
@@ -114,19 +112,24 @@ function checkClient(req,res) {
     }
   }
   if(flag == 0){
-      con.query("SELECT idusers,name,familyname FROM users WHERE Email='"+user_id+"' AND Password='"+pass+"'", function (err, result, fields) {
+    const text = 'SELECT id,name,familyname FROM store."Users" WHERE Email=$1 AND password=$2';
+    const values = [user_id,pass];
+    client.query(text,values, (err, result)=>{
       if (err) throw err;
-      if(result.length > 0){
-
+      if(result != null ){
         res.sendFile(__dirname+ "/Website/index.html");
-        
-        sess.email = req.body.id;
-        sess.pass = req.body.pass;
-        sess.name = result[0].name;
-        sess.lname = result[0].familyname;
+        console.log(result);
+        sess.email = user_id;
+        sess.pass = pass;
+        const data = result.rows;
+        console.log('all data');
+        data.forEach(row => {
+            sess.name = row.name;
+            sess.familyname = row.familyname;
+            console.log(` Hello Id: ${row.id} Name: ${row.name} familyname: ${row.familyname}`);
+        })
         sessions.push(sess);
         res.session = sess;
-        console.log("Successfully logged in , session wil start for : " +result[0].idusers + " Name:" + result[0].name + " "+ result[0].familyname);
       }
       else{
         res.sendFile(__dirname+ "/Website/login.html");
